df093e8dad3aef4d4dd2a7e6c31690c639ba4ab5
Ian D. Eccles <ian.eccles@gmail.com>
2011-04-02 21:44:30 -0400

    failover testing is coming along still a bit more to go, but let's say this fixes #2

--- spec/onstomp/failover/new_with_failover_spec.rb
+++ spec/onstomp/failover/new_with_failover_spec.rb
@@ -2,5 +2,15 @@
 require 'spec_helper'
 
 describe "OnStomp::Client.new with failover", :failover => true do
-
+  describe "monkey patching OnStomp::Client" do
+    it "should return a failover client if given an array" do
+      OnStomp::Client.new(['stomp:///', 'stomp+ssl:///']).should be_a_kind_of(OnStomp::Failover::Client)
+    end
+    it "should return a failover client if given a failover: URI" do
+      OnStomp::Client.new('failover:(stomp:///,stomp+ssl:///)').should be_a_kind_of(OnStomp::Failover::Client)
+    end
+    it "should be a regular client otherwise" do
+      OnStomp::Client.new('stomp:///').should be_a_kind_of(OnStomp::Client)
+    end
+  end
 end
