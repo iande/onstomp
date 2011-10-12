299de5e7fc8aab12fa5a2dfa7fe1a0d739666d53
Ian D. Eccles <ian.eccles@gmail.com>
2011-10-12 19:18:45 -0400

    change destinations for apollo

--- examples/basic.rb
+++ examples/basic.rb
@@ -6,21 +6,21 @@ puts "Starting demo"
 puts "----------------------------"
 
 running = true
-client = OnStomp::Client.new("stomp://localhost")
+client = OnStomp::Client.new("stomp://localhost:61613")
 client.connect
 
 puts "Connected to broker using protocol #{client.connection.version}"
 
-client.subscribe("/queue/onstomp/test") do |message|
+client.subscribe("/queue/onstomp_test") do |message|
   puts "Received: '#{message.body}'"
   if message.body == 'finished'
     running = false
   end
 end
 
-client.send("/queue/onstomp/test", "hello world")
-client.send("/queue/onstomp/test", "this is a simple demo of onstomp")
-client.send("/queue/onstomp/test", "finished")
+client.send("/queue/onstomp_test", "hello world")
+client.send("/queue/onstomp_test", "this is a simple demo of onstomp")
+client.send("/queue/onstomp_test", "finished")
 
 Thread.pass while running
 client.disconnect
