23bd11f3a847772896072ba63ccdd83061b3e215
Ian D. Eccles <ian.eccles@gmail.com>
2014-04-01 09:55:30 -0400

    changes to test environment
    - added more travis targets
    - only do coverage test if SCOV env var is specified

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
 # -*- encoding: utf-8 -*-
 Dir[File.expand_path('support', File.dirname(__FILE__)) + "/**/*.rb"].each { |f| require f }
 
-begin
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "/spec/"
+if ENV['SCOV']
+  begin
+    require 'simplecov'
+    SimpleCov.start do
+      add_filter "/spec/"
+    end
+  rescue LoadError
   end
-rescue LoadError
 end
 
 require 'onstomp'
